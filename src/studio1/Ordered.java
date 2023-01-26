2018256a4ebbb01cb810492d3740caaac6ad822e
kayle <kayle@DESKTOP-AAN93TJ.wusm-wifi.wucon.wustl.edu>
2023-01-26 12:31:16 -0600

    Done

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,7 +15,22 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+		
+		boolean isOrdered = ((x < y) && (y < z))||((x > y) && (y > z));
+		System.out.println(isOrdered);
+		
+		
+		
+		/*if ((x < y) && (y < z)){
+			 isOrdered = true;
+		}
+		if ((x > y) && (y > z)){
+			 isOrdered = true;
+		}
+		else 
+			isOrdered = false;
+		System.out.println(isOrdered);*/
+		
 	}
 
 }
